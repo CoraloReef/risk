5fcd94affd70dc08f0646bbb15e1cab3d5143e24
Viacheslav Gordeiko <slav78rus@gmail.com>
2019-11-19 23:34:09 +0300

    Add tests actions

--- __tests__/actions.test.js
+++ __tests__/actions.test.js
@@ -1,9 +1,109 @@
 import {
+  addContinent,
+  addTerritory,
   setCurrentPlayer,
+  addPlayer,
   setGamePhase,
+  updateTerritoryData,
 } from '../src/actions';
 
 describe('>>> Test Actions', () => {
+  it('test addContinent', () => {
+    const continent = addContinent({
+      continent: {
+        id: 1,
+        name: 'africa',
+        weight: '3',
+        owner: null,
+      },
+    });
+    expect(continent).toEqual({
+      type: 'CONTINENT_ADD',
+      payload: {
+        continent: {
+          id: 1,
+          name: 'africa',
+          weight: '3',
+          owner: null,
+        },
+      },
+    });
+  });
+
+  it('test addTerritory', () => {
+    const territory = addTerritory({
+      territory: {
+        id: 1,
+        name: 'egypt',
+        contacts: ['20', '30', '33', '36'],
+        idContinent: 1,
+        owner: null,
+        armysCount: null,
+      },
+    });
+    expect(territory).toEqual({
+      type: 'TERRITORY_ADD',
+      payload: {
+        territory: {
+          id: 1,
+          name: 'egypt',
+          contacts: ['20', '30', '33', '36'],
+          idContinent: 1,
+          owner: null,
+          armysCount: null,
+        },
+      },
+    });
+  });
+
+  it('test addPlayer', () => {
+    const player = addPlayer({
+      player: {
+        id: 1,
+        color: 'red',
+        name: 'Test name',
+        type: 'human',
+      },
+    });
+    expect(player).toEqual({
+      type: 'PLAYER_ADD',
+      payload: {
+        player: {
+          id: 1,
+          color: 'red',
+          name: 'Test name',
+          type: 'human',
+        },
+      },
+    });
+  });
+
+  it('test updateTerritoryData', () => {
+    const territory = updateTerritoryData({
+      territory: {
+        id: 1,
+        name: 'egypt',
+        contacts: ['20', '30', '33', '36'],
+        idContinent: 1,
+        owner: 1,
+        armysCount: 2,
+      },
+    });
+    expect(territory).toEqual({
+      type: 'TERRITORY_DATA_UPDATE',
+      payload: {
+        territory: {
+          id: 1,
+          name: 'egypt',
+          contacts: ['20', '30', '33', '36'],
+          idContinent: 1,
+          owner: 1,
+          armysCount: 2,
+        },
+      },
+    });
+  });
+
   it('test setCurrentPlayer', () => {
     const id = setCurrentPlayer(1);
     expect(id).toEqual({ type: 'CURRENT_PLAYER_SET', payload: 1 });
