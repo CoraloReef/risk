4100af7a328717cdc307b325ee31c154fab7d8d9
Viacheslav Gordeiko <slav78rus@gmail.com>
2019-11-21 17:21:11 +0300

    Refactor small

--- src/app.jsx
+++ src/app.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import { configureStore } from '@reduxjs/toolkit';
 import { loadTranslations, setLocale, syncTranslationWithStore } from 'react-redux-i18n';
@@ -18,7 +18,7 @@ store.dispatch(loadTranslations(translationsObject));
 store.dispatch(setLocale('ru'));
 
 export default () => {
-  ReactDOM.render(
+  render(
     <Provider store={store}>
       <App />
     </Provider>,
