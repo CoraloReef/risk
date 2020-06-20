a37b1dbefb8b035a02bf7273a98e575af4d63567
Viacheslav Gordeiko <slav78rus@gmail.com>
2020-06-21 00:40:23 +0300

    Changed infoPanel to functional component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,40 +1,48 @@
-import React from 'react';
-import { Container } from 'react-bootstrap';
+import React, { Suspense } from 'react';
+import { connect } from 'react-redux';
 
-import connect from '../connect';
+import Container from 'react-bootstrap/Container';
+
 import Header from './Header';
 import StartSettings from './StartSettings';
 import Polygon from './Polygon';
 import InfoPanel from './InfoPanel';
+import Loader from './Loader';
 
 const mapStateToProps = (state) => {
   const { gamePhase } = state;
   return { gamePhase };
 };
+const actionCreators = {};
 
-@connect(mapStateToProps)
-class App extends React.Component {
-  render() {
-    const { gamePhase } = this.props;
+const Component = (props) => {
+  const { gamePhase } = props;
 
-    return (
-      <>
-        <Header />
-        <section>
-          <Container className="mb-4">
-            {gamePhase === 'start' ? (
-              <StartSettings />
-            ) : (
-              <>
-                <Polygon />
-                <InfoPanel />
-              </>
-            )}
-          </Container>
-        </section>
-      </>
-    );
-  }
-}
+  return (
+    <>
+      <Header />
+      <section>
+        <Container className="mb-4">
+          {gamePhase === 'start' ? (
+            <StartSettings />
+          ) : (
+            <>
+              <Polygon />
+              <InfoPanel />
+            </>
+          )}
+        </Container>
+      </section>
+    </>
+  );
+};
+
+const ConnectComponent = connect(mapStateToProps, actionCreators)(Component);
+
+const App = () => (
+  <Suspense fallback={<Loader />}>
+    <ConnectComponent />
+  </Suspense>
+);
 
 export default App;
