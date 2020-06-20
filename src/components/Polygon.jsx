55ca7efbd11e9598d4c7ed4efb59a2c32dd0fe98
Viacheslav Gordeiko <slav78rus@gmail.com>
2020-06-21 00:35:36 +0300

    Changed Polygon to functional component

--- src/components/Polygon.jsx
+++ src/components/Polygon.jsx
@@ -1,23 +1,32 @@
-import React from 'react';
-import { Row, Col } from 'react-bootstrap';
+import React, { useEffect, Suspense } from 'react';
+import { connect } from 'react-redux';
 
-import connect from '../connect';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
 import territoriesObj from '../territories.json';
 import Ceil from './Ceil';
+import Loader from './Loader';
+
+import { actions } from '../slices';
 
 const mapStateToProps = (state) => {
-  const { territories: { byId, allIds } } = state;
+  const {
+    territories: { byId, allIds },
+  } = state;
   const territories = allIds.map((id) => byId[id]);
 
   return { territories };
 };
+const actionCreators = {
+  addTerritory: actions.addTerritory,
+  addContinent: actions.addContinent,
+};
 
-@connect(mapStateToProps)
+const Component = (props) => {
+  const { territories, addContinent, addTerritory } = props;
 
-class Polygon extends React.Component {
-  componentDidMount() {
-    const { addContinent, addTerritory } = this.props;
-
+  useEffect(() => {
     territoriesObj.continents.forEach((continent) => {
       const continetData = {
         id: continent.id,
@@ -41,50 +50,51 @@ class Polygon extends React.Component {
 
       addContinent({ continent: continetData });
     });
-  }
+  }, []);
 
-  render() {
-    const { territories } = this.props;
+  return (
+    <Row>
+      <Col className="polygon mb-2">
+        {territories.map((territory) => (
+          <Ceil id={territory.id} key={territory.id} />
+        ))}
+        <div className="line greenland-1" />
+        <div className="line greenland-2" />
+        <div className="line greenland-3" />
+        <div className="line greenland-4" />
+        <div className="line iceland-1" />
+        <div className="line iceland-2" />
+        <div className="line great-britain-1" />
+        <div className="line great-britain-2" />
+        <div className="line great-britain-3" />
+        <div className="line scandinavia-1" />
+        <div className="line brazil-1" />
+        <div className="line north-africa-1" />
+        <div className="line north-africa-2" />
+        <div className="line egypt-1" />
+        <div className="line east-africa-1" />
+        <div className="line madagascar-1" />
+        <div className="line madagascar-2" />
+        <div className="line japan-1" />
+        <div className="line japan-2" />
+        <div className="line kamchatka-1" />
+        <div className="line alaska-1" />
+        <div className="line siam-1" />
+        <div className="line indonesia-1" />
+        <div className="line indonesia-2" />
+        <div className="line new-guinea-1" />
+        <div className="line new-guinea-2" />
+      </Col>
+    </Row>
+  );
+};
 
-    return (
-      <Row>
-        <Col className="polygon mb-2">
-          {territories.map((territory) => (
-            <Ceil
-              id={territory.id}
-              key={territory.id}
-            />
-          ))}
-          <div className="line greenland-1" />
-          <div className="line greenland-2" />
-          <div className="line greenland-3" />
-          <div className="line greenland-4" />
-          <div className="line iceland-1" />
-          <div className="line iceland-2" />
-          <div className="line great-britain-1" />
-          <div className="line great-britain-2" />
-          <div className="line great-britain-3" />
-          <div className="line scandinavia-1" />
-          <div className="line brazil-1" />
-          <div className="line north-africa-1" />
-          <div className="line north-africa-2" />
-          <div className="line egypt-1" />
-          <div className="line east-africa-1" />
-          <div className="line madagascar-1" />
-          <div className="line madagascar-2" />
-          <div className="line japan-1" />
-          <div className="line japan-2" />
-          <div className="line kamchatka-1" />
-          <div className="line alaska-1" />
-          <div className="line siam-1" />
-          <div className="line indonesia-1" />
-          <div className="line indonesia-2" />
-          <div className="line new-guinea-1" />
-          <div className="line new-guinea-2" />
-        </Col>
-      </Row>
-    );
-  }
-}
+const ConnectComponent = connect(mapStateToProps, actionCreators)(Component);
+
+const Polygon = () => (
+  <Suspense fallback={<Loader />}>
+    <ConnectComponent />
+  </Suspense>
+);
 
 export default Polygon;
