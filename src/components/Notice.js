af07a374e3fbaf5a1ce59e00bd7a8d81b6a0453c
Viacheslav Gordeiko <slav78rus@gmail.com>
2020-08-04 16:28:02 +0300

    Added Notice style position

--- src/components/Notice.js
+++ src/components/Notice.js
@@ -8,13 +8,32 @@ const Notice = (props) => {
   const { heading, smallHeading, text } = props;
 
   return (
-    <Toast onClose={() => setShow(false)} show={show} delay={3000} autohide>
-      <Toast.Header>
-        <strong className="mr-auto">{heading}</strong>
-        <small>{smallHeading}</small>
-      </Toast.Header>
-      <Toast.Body>{text}</Toast.Body>
-    </Toast>
+    <div
+      aria-live="polite"
+      aria-atomic="true"
+      style={{
+        position: 'relative',
+        minHeight: '100px',
+      }}
+    >
+      <Toast
+        style={{
+          position: 'absolute',
+          top: 0,
+          right: 0,
+        }}
+        onClose={() => setShow(false)}
+        show={show}
+        delay={3000}
+        autohide
+      >
+        <Toast.Header>
+          <strong className="mr-auto">{heading}</strong>
+          <small>{smallHeading}</small>
+        </Toast.Header>
+        <Toast.Body>{text}</Toast.Body>
+      </Toast>
+    </div>
   );
 };
 
